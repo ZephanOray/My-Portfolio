dc1705389819708491ca6f673fb14609f84288a9
zephan.goodwin <USER_EMAIL>
2025-01-14 21:15:43 -0500

    Add project files.

---
+++ My Portfolio/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace My_Portfolio
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var AAPL = new Stock("Apple", 240.00, 225.00);
+
+            AAPL.PriceChange();
+        }
+    }
+}
---
+++ My Portfolio/Stock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace My_Portfolio
+{
+    internal class Stock
+    {
+        public string StockName { get; set; }
+        public double OpeningPrice { get; set; }
+        public double ClosingPrice { get; set; }
+
+        public Stock(string stockName, double openingPrice, double closingPrice)
+        {
+            StockName = stockName;
+            OpeningPrice = openingPrice;
+            ClosingPrice = closingPrice;
+        }
+
+        public void PriceChange()
+        {
+            string result = ClosingPrice > OpeningPrice ? $"Down {(ClosingPrice - OpeningPrice).ToString("C2")}" : $"Up {(OpeningPrice - ClosingPrice).ToString("C2")}";
+
+            Console.WriteLine(result);
+        }
+
+        public void AddStock()
+        {
+
+        }
+
+
+    }
+}
